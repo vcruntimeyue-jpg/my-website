f3e5832525e91b0b363358aaa007e1663f7f270c
vcrunyue <vcruntimeyue@gmail.com>
2026-04-06 13:44:31 +0800

    fix: reorder social icons and scale gmail

--- app/components/SocialIcon.js
+++ app/components/SocialIcon.js
@@ -11,7 +11,7 @@ const TwitterIcon = () => (
 );
 
 const GmailIcon = () => (
-  <svg aria-hidden="true" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="1em" height="1em" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+  <svg aria-hidden="true" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="1.05em" height="1.05em" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
     <rect x="3" y="5" width="18" height="14" rx="2" />
     <path d="M3 7l9 6 9-6" />
   </svg>
