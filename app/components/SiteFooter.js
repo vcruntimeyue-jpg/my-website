fb7e9bd6ce251e421480431f70958b8796c519ac
vcrunyue <vcruntimeyue@gmail.com>
2026-04-08 15:35:27 +0800

    refine: tighten footer card spacing

--- app/components/SiteFooter.js
+++ app/components/SiteFooter.js
@@ -2,7 +2,7 @@ import SocialLinks from "./SocialLinks";
 
 export default function SiteFooter({ siteName, socialLinks }) {
   return (
-    <footer className="mt-0 pt-4 text-center font-fantasy text-2xl text-gray-400" style={{ paddingBottom: "50px" }}>
+    <footer className="mt-0 pt-4 text-center font-fantasy text-2xl text-gray-400" style={{ paddingBottom: "0px" }}>
       {siteName}
       <div className="mt-4 flex items-center justify-center gap-x-6 text-2xl">
         <SocialLinks
